48f03bda6154f232626965bb642b46d709ec2300
Gustavo Bachesque <gustavobachesque10@outlook.com>
2024-05-11 02:09:27 -0300

    Projeto finalizado

--- conta-banco/README.md
+++ conta-banco/README.md
@@ -1,6 +1,6 @@
-## Getting Started
+## Conta Bancária
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Resolução do desafio Bootcamp Santander Backend com Java 2024
 
 ## Folder Structure
 
@@ -9,10 +9,8 @@ The workspace contains two folders by default, where:
 - `src`: the folder to maintain sources
 - `lib`: the folder to maintain dependencies
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+ 
 
-## Dependency Management
 
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Bem vindo, siga as intruções para criação de sua conta no banco.");
+
+        System.out.print("Por favor, insira o seu nome: ");
+        String name = sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int accountNumber = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Digite o número da Agência: ");
+        String agency = sc.nextLine();
+        
+        System.out.print("Insira o saldo que deseja depositar: ");
+        double balance = sc.nextDouble();
+
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + accountNumber + " e seu saldo " + balance + " já está disponível para saque.");
+             
+        
+        sc.close();
     }
 }
